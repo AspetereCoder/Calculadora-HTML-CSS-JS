d806610e7dede3406d30f7e4ca86a2967fab4665
Aspetere <pedrinhoalgusto07@gmail.com>
2022-11-13 20:25:28 -0300

    versao 1

---
+++ script.js
@@ -0,0 +1,49 @@
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const display = document.querySelector("#display");
+const equal = document.querySelector(".operator-eq");
+const clean = document.querySelector("#clean");
+const del = document.querySelector("#del");
+
+//  adding functionality to numbers
+
+numbers.forEach(function (value) {
+    value.addEventListener("click", () => display.value += value.innerHTML)
+});
+
+// adding functionality to operators 
+
+operators.forEach(function (value) {
+    value.addEventListener("click", function () {
+        let lastChar = display.value[display.value.length - 1];
+        
+        switch (lastChar) {
+            case '÷':
+                return;
+            case '×':
+                return;
+            case '+':
+                return;
+            case '-':
+                return;
+            default:
+                display.value += value.innerHTML;
+        }
+    })
+});
+
+// adding functionality to equal operator
+
+equal.addEventListener("click", function () {
+    let formatedString = display.value.replace('×', '*');
+    formatedString = stringToEval.replace('÷', '/');
+    display.value = eval(formatedString);
+});
+
+clean.addEventListener("click", function () {
+    display.value = "";
+});
+
+del.addEventListener("click", function () {
+    display.value = display.value.replace(display.value[display.value.length - 1], '');
+});
